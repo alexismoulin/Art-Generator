08676ca350fa87c8b63b0be65999e3dc898336d0
Alexis Moulin <alexis.moulin@icloud.com>
2022-12-25 15:24:47 +0100

    code refactoring and cleaning

--- Art Generator/ContentView.swift
+++ Art Generator/ContentView.swift
@@ -4,49 +4,39 @@ struct ContentView: View {
     
     @State private var prompt: String = ""
     @State private var responseURL: URL?
-    
-    var apiKey: String? {
-        Bundle.main.infoDictionary?["IMAGE_API_KEY"] as? String
-    }
-    
-    struct MyJsonObject: Encodable {
-        let prompt: String
-    }
-    
-    struct DecodedImage: Decodable {
-        var created: Int
-        var data: [ImageData]
-    }
-    
-    struct ImageData: Decodable {
-        var url: String
-    }
-    
-    func postRequest() async throws {
-        let myJsonObject = MyJsonObject(prompt: prompt)
-        let payload = try JSONEncoder().encode(myJsonObject)
-        guard let url = URL(string: "https://api.openai.com/v1/images/generations") else {
-            fatalError("Missing URL")
-        }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.httpMethod = "POST"
-        urlRequest.setValue("Bearer \(apiKey!)", forHTTPHeaderField: "Authentication")
-        
-        let (data, response) = try await URLSession.shared.upload(for: urlRequest, from: payload)
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-            fatalError("Error while fetching data")
-        }
-        let decodedImage = try JSONDecoder().decode(DecodedImage.self, from: data)
-        responseURL = URL(string: decodedImage.data[0].url)
-    }
+    @State private var displayImage: Bool = false
     
     var body: some View {
         VStack {
             TextField("Test", text: $prompt)
-            Button("Post") { }
-            AsyncImage(url: responseURL)
+                .textFieldStyle(.roundedBorder)
+            Button("Post") {
+                Task {
+                    do {
+                        displayImage = true
+                        responseURL = await URL(string: try NetworkRequest.postRequest(prompt: prompt))
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            if displayImage {
+                AsyncImage(url: responseURL) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 300, maxHeight: 300)
+                    case .failure:
+                        Image(systemName: "photo")
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+            }
         }
         .padding()
     }
